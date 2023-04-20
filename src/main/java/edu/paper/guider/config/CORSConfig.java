2b7794554f2712d304029d780663bbc6c2d69a8f
Burnysheff <77582124+Burnysheff@users.noreply.github.com>
2023-04-20 10:28:41 +0300

    CORS url fixed

--- src/main/java/edu/paper/guider/config/CORSConfig.java
+++ src/main/java/edu/paper/guider/config/CORSConfig.java
@@ -10,7 +10,7 @@ public class CORSConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://master--superb-moonbeam-048cf3.netlify.app")
+                //.allowedOrigins("https://master--superb-moonbeam-048cf3.netlify.app")
                 .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH");
     }
 
